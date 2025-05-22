995f812efc8113fd6afb69d13075b228d948dbd3
Glenn Palic <noexgp@gmail.com>
2025-05-22 12:16:21 +0800

    Update product Description in product carousel

--- components/shared/product/product-carousel.tsx
+++ components/shared/product/product-carousel.tsx
@@ -42,7 +42,7 @@ const ProductCarousel = ({ data }: { data: Product[] }) => {
                 />
                 <div className='absolute inset-0 flex items-end justify-center'>
                   <h2 className='bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white'>
-                    asdfasdf
+                    {product.description}
                   </h2>
                 </div>
               </div>
